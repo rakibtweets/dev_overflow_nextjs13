ee50a59070d2971f796f34b3ee560e830e9b2844
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-10-10 16:26:11 +0600

    add right-sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/shared/navbar/Navbar';
 import React from 'react';
 import LeftSidebar from '../../components/shared/LeftSidebar/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar/RightSidebar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 px-6 pt-36 max-md:pb-14 sm:px-14 ">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        Right Sidebar
+        <RightSidebar />
       </div>
       Toster component
     </main>
---
+++ components/shared/RightSidebar/RenderTag.tsx
@@ -0,0 +1,30 @@
+// interface example
+
+import Link from 'next/link';
+import { Badge } from '@/components/ui/badge';
+
+interface RenderTagProps {
+  _id: number;
+  name: string;
+  totalQuestions?: number;
+  showCount?: boolean;
+}
+
+const RenderTag = ({
+  _id,
+  name,
+  totalQuestions,
+  showCount
+}: RenderTagProps) => {
+  return (
+    <Link href={`/tags/${_id}`} className="flex justify-between gap-3">
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+        {name}
+      </Badge>
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{totalQuestions}</p>
+      )}
+    </Link>
+  );
+};
+export default RenderTag;
