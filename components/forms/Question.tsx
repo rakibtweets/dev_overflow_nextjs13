910cffd50a6173245577b84296dfd890da0da6e2
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-10-16 21:28:09 +0600

    tinymce editor added

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -18,6 +21,7 @@ import { Input } from '@/components/ui/input';
 import { questionsSchema } from '@/lib/validations';
 
 const Question = () => {
+  const editorRef = useRef(null);
   // 1. Define your form.
   const form = useForm<z.infer<typeof questionsSchema>>({
     resolver: zodResolver(questionsSchema),
@@ -34,6 +38,7 @@ const Question = () => {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
   return (
     <Form {...form}>
       <form
@@ -73,6 +78,40 @@ const Question = () => {
               </FormLabel>
               <FormControl className="mt-3.5 ">
                 {/* //todo: add an editor component */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor;
+                  }}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      'advlist',
+                      'autolink',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'codesample',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table'
+                    ],
+                    toolbar:
+                      'undo redo |  ' +
+                      'codesample | bold italic forecolor | alignleft aligncenter ' +
+                      'alignright alignjustify | bullist numlist ',
+                    content_style: 'body { font-family:Inter; font-size:16px }'
+                  }}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you put in the title.
