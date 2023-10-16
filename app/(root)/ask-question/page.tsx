bdd6cd22a05119caee9f003e04f9e4091765c3e0
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-10-16 20:34:58 +0600

    validate question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,4 +1,13 @@
+import Question from '@/components/forms/Question';
+
 const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 export default AskQuestion;
