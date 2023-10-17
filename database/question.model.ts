82da85a0bba088ff24bd17e010e0e71ab2d70e63
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-10-17 23:47:24 +0600

    db: question model added

---
+++ database/question.model.ts
@@ -0,0 +1,68 @@
+import { Schema, models, model, Document } from 'mongoose';
+
+// create mongoose schema for question in typescript
+
+// question interface extends mongoose document
+
+export interface IQuestion extends Document {
+  title: string;
+  content: string;
+  tags: Schema.Types.ObjectId[];
+  views: number;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId;
+  answers: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const questionSchema = new Schema<IQuestion>({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  tags: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Tag'
+    }
+  ],
+  views: {
+    type: Number,
+    default: 0
+  },
+  upvotes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  downvotes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  answers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Answer'
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Question = models.Question || model('Question', questionSchema);
+
+export default Question;
