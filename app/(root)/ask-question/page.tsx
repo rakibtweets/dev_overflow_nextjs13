c2926e19ca1ac2e01350a6031045d4cdf33e7db6
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-10-20 02:10:04 +0600

    config next image

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,11 @@
 import Question from '@/components/forms/Question';
 import { getUserById } from '@/lib/actions/user.action';
-// import { auth } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId = 12345;
+  const { userId } = auth();
+
   if (!userId) redirect('/sign-in');
 
   const mongoUser = await getUserById({ userId });
--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -57,7 +57,7 @@ const QuestionCard = ({
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
           href={`/profile/${author._id}`}
-          imgUrl={'/assets/icons/avatar.svg'}
+          imgUrl={author.picture}
           alt="user"
           value={author.name}
           isAuthor
