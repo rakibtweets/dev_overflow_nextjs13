266887e157f27bdf8857ced18f89b60d77fed27f
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-10-24 18:26:40 +0600

    feat: display all savedQuestions

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,26 +1,22 @@
 import LocalSearchBar from '@/components/shared/search/LocalSearchBar';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { HomePageFilters } from '@/constants/filters';
+import { QuestionFilters } from '@/constants/filters';
 import Filter from '@/components/shared/Filter/Filter';
-import HomeFilters from '@/components/home/HomeFilters';
 import NoResult from '@/components/shared/NoResult/NoResult';
 import QuestionCard from '@/components/cards/QuestionCard';
-import { getQuestions } from '@/lib/actions/question.action';
+import { getSavedQuestions } from '@/lib/actions/user.action';
+import { auth } from '@clerk/nextjs';
 
-export default async function Home() {
-  const result = await getQuestions({});
+export default async function Collection() {
+  const { userId } = auth();
+  if (!userId) return null;
+  const { questions } = await getSavedQuestions({
+    clerkId: userId
+  });
 
   return (
     <>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-        <Link href={'/ask-question'} className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[40px] rounded-lg px-4 py-3 !text-light-900">
-            Ask a Question
-          </Button>
-        </Link>
-      </div>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchBar
           route="/"
@@ -31,19 +27,16 @@ export default async function Home() {
         />
 
         <Filter
-          filters={HomePageFilters}
+          filters={QuestionFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses="hidden max-md:flex"
         />
       </div>
-
-      <HomeFilters />
 
       {/* Card section */}
       <div className="mt-10 flex flex-col gap-6">
         {/*  //!Todo: looping through questions */}
-        {result.questions.length > 0 ? (
-          result.questions?.map((question) => (
+        {questions.length > 0 ? (
+          questions?.map((question: any) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
@@ -58,7 +51,7 @@ export default async function Home() {
           ))
         ) : (
           <NoResult
-            title="There is no quesiton to show"
+            title="There is no saved quesiton to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,16 +2,19 @@
 'use server';
 
 import User from '@/database/user.model';
+import { FilterQuery } from 'mongoose';
 import { connectToDatabase } from '../db/mongoose';
 import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   ToggleSaveQuestionParams,
   UpdateUserParams
 } from './shared.types';
 import { revalidatePath } from 'next/cache';
 import Question from '@/database/question.model';
+import Tag from '@/database/tag.model';
 
 export const getUserById = async (params: any) => {
   try {
@@ -141,6 +144,40 @@ export const toggleSaveQuestion = async (params: ToggleSaveQuestionParams) => {
     }
 
     revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getSavedQuestions = async (params: GetSavedQuestionsParams) => {
+  try {
+    connectToDatabase();
+    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, 'i') } }
+      : {};
+
+    const user = await User.findOne({ clerkId }).populate({
+      path: 'saved',
+      match: query,
+      options: {
+        sort: { createdAt: -1 }
+      },
+      populate: [
+        { path: 'tags', model: Tag, select: '_id name' },
+        { path: 'author', model: User, select: '_id clerkId name picture' }
+      ]
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const saveQuestions = user.saved;
+
+    return { questions: saveQuestions };
   } catch (error) {
     console.log(error);
     throw error;
