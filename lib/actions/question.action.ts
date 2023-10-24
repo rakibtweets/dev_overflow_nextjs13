7700268d6a16464fc092635878daca8b06945b51
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-10-24 10:14:02 +0600

    Upvote/DownVote actions for Question

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -6,7 +6,8 @@ import Tag from '@/database/tag.model';
 import {
   CreateQuestionParams,
   GetQuestionByIdParams,
-  GetQuestionsParams
+  GetQuestionsParams,
+  QuestionVoteParams
 } from './shared.types';
 import User from '@/database/user.model';
 import { revalidatePath } from 'next/cache';
@@ -88,6 +89,78 @@ export const getQuestionById = async (params: GetQuestionByIdParams) => {
       });
 
     return { question };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const upvoteQuestion = async (params: QuestionVoteParams) => {
+  try {
+    connectToDatabase();
+
+    const { questionId, userId, hasupVoted, path, hasdownVoted } = params;
+
+    let updateQuery = {};
+    if (hasupVoted) {
+      updateQuery = { $pull: { upvotes: userId } };
+    } else if (hasdownVoted) {
+      updateQuery = {
+        $pull: { downvotes: userId },
+        $push: { upvotes: userId }
+      };
+    } else {
+      updateQuery = { $addToSet: { upvotes: userId } };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true
+    });
+    if (!question) {
+      throw new Error('No question found');
+    }
+
+    // Todo: increment author's reputation
+
+    revalidatePath(path);
+
+    // get user by id
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const downVoteQuestion = async (params: QuestionVoteParams) => {
+  try {
+    connectToDatabase();
+
+    const { questionId, userId, hasupVoted, path, hasdownVoted } = params;
+
+    let updateQuery = {};
+    if (hasdownVoted) {
+      updateQuery = { $pull: { downvotes: userId } };
+    } else if (hasupVoted) {
+      updateQuery = {
+        $pull: { upvotes: userId },
+        $push: { downvotes: userId }
+      };
+    } else {
+      updateQuery = { $addToSet: { downvotes: userId } };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true
+    });
+    if (!question) {
+      throw new Error('No question found');
+    }
+
+    // Todo: increment author's reputation
+
+    revalidatePath(path);
+
+    // get user by id
   } catch (error) {
     console.log(error);
     throw error;
