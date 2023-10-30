38dd7e3de7fd01dddd87ed5f45ca14f6f91bd873
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-10-30 12:51:44 +0600

    manage Home fileter page

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -2,9 +2,35 @@
 
 import { HomePageFilters } from '@/constants/filters';
 import { Button } from '../ui/button';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+import { formUrlQuery } from '@/lib/utils';
 
 const HomeFilters = () => {
-  const active = 'recommended';
+  const [active, setActive] = useState('');
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleClick = (item: string) => {
+    if (active === item) {
+      setActive('');
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'filter',
+        value: null
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'filter',
+        value: item.toLowerCase()
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((item) => (
@@ -15,7 +41,7 @@ const HomeFilters = () => {
               : 'bg-light-800 text-light-500'
           } body-medium rounded-lg px-6 py-3 capitalize shadow-none`}
           key={item.value}
-          onClick={() => {}}
+          onClick={() => handleClick(item.value)}
         >
           {item.name}
         </Button>
